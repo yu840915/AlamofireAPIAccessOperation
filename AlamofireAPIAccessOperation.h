9a59f63221dd9a3471fa1b4ea7af637404c13fbb
yu840915 <yu840915@gmail.com>
2019-03-25 11:35:54 +0800

    Init

---
+++ AlamofireAPIAccessOperation.h
@@ -0,0 +1,19 @@
+//
+//  AlamofireAPIAccessOperation.h
+//  AlamofireAPIAccessOperation
+//
+//  Created by 立宣于 on 2019/3/25.
+//  Copyright © 2019 立宣于. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlamofireAPIAccessOperation.
+FOUNDATION_EXPORT double AlamofireAPIAccessOperationVersionNumber;
+
+//! Project version string for AlamofireAPIAccessOperation.
+FOUNDATION_EXPORT const unsigned char AlamofireAPIAccessOperationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireAPIAccessOperation/PublicHeader.h>
+
+
