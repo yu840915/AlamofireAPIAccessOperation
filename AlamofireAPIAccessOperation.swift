0731b8a10facff74fa5f04899ff7cb2b62efc5f8
yu840915 <yu840915@gmail.com>
2019-03-25 11:50:21 +0800

    Set up

---
+++ AlamofireAPIAccessOperation.swift
@@ -0,0 +1,15 @@
+//
+//  AlamofireAPIAccessOperation.swift
+//  AlamofireAPIAccessOperation
+//
+//  Created by 立宣于 on 2019/3/25.
+//  Copyright © 2019 立宣于. All rights reserved.
+//
+
+import Foundation
+import ModelBlocks
+import Alamofire
+
+class AlamofireAPIAccessOperation: SimpleAsynchronousOperation {
+    
+}
