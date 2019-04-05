34214292dae49f3efb02cb256dd241ccb7758114
yu840915 <yu840915@gmail.com>
2019-04-06 00:51:25 +0800

    Remove header

--- AlamofireAPIAccessOperation.swift
+++ AlamofireAPIAccessOperation.swift
@@ -1,11 +1,3 @@
-//
-//  AlamofireAPIAccessOperation.swift
-//  AlamofireAPIAccessOperation
-//
-//  Created by 立宣于 on 2019/3/25.
-//  Copyright © 2019 立宣于. All rights reserved.
-//
-
 import Foundation
 import ModelBlocks
 import Alamofire
